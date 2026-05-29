30717e9ee863349acb3933fed15e2339f554b2c7
María Victoria <maria.arteagare@campusucc.edu.co>
2026-05-29 02:09:08 -0500

    Perfil de usuario

--- src/types/auth.types.ts
+++ src/types/auth.types.ts
@@ -11,5 +11,14 @@ export interface RegisterRequestDTO {
 
 export interface AuthResponseDTO {
   token: string;
-  message?: string; 
+  message?: string;
+}
+
+export interface UserProfileDTO {
+  id: number;
+  username: string;
+  email: string;
+  role: string;
+  recoveryEmail: string | null;
+  twoFactorEnabled: boolean;
 }
